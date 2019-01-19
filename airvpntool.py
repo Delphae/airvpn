d9e32f752063b8a8dfb7b53a77d94562a14dc398
delphae <45516472+Delphae@users.noreply.github.com>
2019-01-19 18:51:39 +0100

    Add files via upload

--- airvpntool.py
+++ airvpntool.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
+from __future__ import print_function
+
 
 #    ____       _       _
 #   |  _ \  ___| |_ __ | |__   __ _  ___
@@ -10,17 +12,27 @@
 
 
 MAXROWS = 40
+#DEVICE  = 'nuc'
 
-__version__ = '3.6'
-__date__ = '2018-11-18'
+__version__ = '3.12'
+__date__ = '2019-01-19'
 
-import sys, random
 
+'''
+TODO
+
+
+'''
+
+import sys, urllib2, json
+import socket
 from AirPy import Airvpn
-from hostanalyse import read_speedtestlog
+from datetime import timedelta
+from datetime import datetime as dt
 
 def listservers(servers):
-    serversort = sorted(servers, key=lambda x:int(x.currentload), reverse=False)
+    #serversort = sorted(servers, key=lambda x:int(x.currentload), reverse=False)
+    serversort = sorted(servers, key=lambda x:x.public_name, reverse=False)
     print ('%2s %-12s %s %3s %3s   %-18s %-18s' % ('#', 'server', 'flag', 'load', 'user', 'ip' ,'ip-alt'))
     for k, server in enumerate(serversort[:MAXROWS]):
         print ('%2d %-14s %2s  %3d %4d   %-18s %-18s' %
@@ -35,74 +47,84 @@ def listservers(servers):
                )
     print ('%2d Count' % len(servers))
 
+#%%
+def myip():
+    opener = urllib2.build_opener()
+    opener.addheaders = [('User-Agent', 'Mozilla/5.0')]
+    response = opener.open('http://ifconfig.co/ip')
+    ipv4 = response.read()[:-1]
+    return ipv4
+#%%
+def session2json(session):
+    sessiondict = dict(timestamp=dt.now().strftime('%Y-%m-%d %H:%M:%S'),
+                  device=session.device_name,
+                  server=session.server_name,
+                  ipv4=session.exit_ipv4,
+                  ipv6=session.exit_ipv6)
+    jsdata = json.dumps(sessiondict)
+    f = open('ipaddresslog.json', 'a')
+    f.write('%s\n' % jsdata)
+    f.close()
+    return
 
-def usersummary():
-    print ('%-19s %s' % ('Login', air.user.login))
-    print ('%-19s %s' % ('Connected', air.user.connected))
+
+#%%
+def usersummary(air, ipaddress):
     if air.user.connected:
-        for k, session in enumerate(air.sessions):
-            print
-            print ('%-19s %1d' % ('Session', k+1))
+        for k, session in enumerate(sorted(air.sessions, key=lambda ses:ses.device_name, reverse=False)):
+            session2json(session)
+            if session.device_name==socket.gethostname():
+                marker='*'
+            else:
+                marker=''
+
+#            print ('%-19s %1d%s' % ('Session', k+1, marker))
+            print ('%-19s %s%s' % ('Device', session.device_name, marker))
             print ('%-19s %s' % ('Server', session.server_name))
-            print ('%-19s %s' % ('Entry ip', session.entry_ip))
-            print ('%-19s %s' % ('Exit ip', session.exit_ip))
+
+            print ('%-19s %s' % ('IPv4', session.exit_ipv4))
+            print ('%-19s %s' % ('IPv6', session.exit_ipv6))
             print ('%-19s %9.3f MB' % ('Read', int(session.bytes_read)/1024.0/1024.0))
             print ('%-19s %9.3f MB' % ('Write', int(session.bytes_write)/1024.0/1024.0))
-            print ('%-19s %s UTC' % ('Connected since', session.connected_since_date))
-            print ('%-19s %s' % ('Last activity', air.user.last))
+            cettime = session.connected + timedelta(hours=1)
+            print ('%-19s %s CET' % ('Connected since', cettime))
+            uptime = dt.now()-cettime
+            hours = uptime.seconds // 3600
+            minutes = uptime.seconds % 3600 // 60
+            print ('%-19s days=%s, hrs=%s, min=%s' % ('uptime',
+                                                      uptime.days,
+                                                      hours,
+                                                      minutes))
             print ('%-19s %s, %s' % ('Location', session.server_location,
                                                  session.server_country_code.upper()))
+            print ()
+    return
 
-def lowest(servers):
-    speedlog = read_speedtestlog()
-    speednodes = [speedtest.node for speedtest in speedlog]
-    
-    counts = [(server, speednodes.count(server.public_name)) for server in servers]
-    countmin = min([count[1] for count in counts])
-    candidates = [count[0] for count in counts if count[1]==countmin]
-    return random.choice(candidates)
+def nodesummary(air, ipv4):
+    if air.user.connected:
+        for session in air.sessions:
+            if session.device_name==socket.gethostname():
+                print (session.server_name)
+    else:
+        print('not connected')
+    return
 
 
 if __name__ == '__main__':
-    verbose = '-v' in sys.argv
-    rand    = '-r' in sys.argv
     count   = '-c' in sys.argv
     report  = '-r' in sys.argv
     user    = '-u' in sys.argv
     node    = '-n' in sys.argv
-    low     = '-l' in sys.argv
 
     apikey = open('airapikey.txt').read()
     air = Airvpn(apikey, 'NL')
+    ipv4 = myip()
 
-    if rand:
-        txt = 'random' if verbose else ''
-        server = air.rand()
-    elif low:
-        txt = 'lowest' if verbose else ''
-        server = lowest(air.servers())
-    else:
-        txt = 'best' if verbose else ''
-        server = air.best()
-
-    if verbose:
-        print ('%-10s %s' % ('Select',txt))
-        print ('%-10s %s' % ('Server',server.public_name))
-        print ('%-10s %s' % ('Load',server.currentload))
-        print ('%-10s %s' % ('Users',server.users))
-        print ('%-10s %s' % ('IP',server.ip_entry))
-        print ('%-10s %s' % ('IP_alt',server.ip_entry_alt))
-        print ('%-10s %s' % ('Country',server.country_code))
-    elif count:
+    if count:
         print (len(air.servers()))
     elif report:
         listservers(air.servers())
     elif user:
-        usersummary()
+        usersummary(air, ipv4)
     elif node:
-        if air.user.connected:
-            print (air.connection.server_name)
-        else:
-            print('not connected')
-    else:
-        print (server.public_name)
+        nodesummary(air, ipv4)
